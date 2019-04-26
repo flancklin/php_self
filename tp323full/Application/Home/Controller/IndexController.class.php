a06fc60af81de96e93783c8b0614fbd364318838
componey_qss <475185283@qq.com>
2019-04-26 16:46:22 +0800

    tp323full传值给view以及view展示 Signed-off-by: componey_qss <475185283@qq.com>

--- tp323full/Application/Home/Controller/IndexController.class.php
+++ tp323full/Application/Home/Controller/IndexController.class.php
@@ -1,7 +1,13 @@
 <?php
+
 namespace Home\Controller;
+
 use Think\Controller;
-class IndexController extends Controller {
+
+class IndexController extends Controller{
+    /**
+     * tp323full渲染方式
+     */
     public function index(){
         $this->assign("sayHello", "hello world");
         $this->display();
@@ -10,9 +16,9 @@ class IndexController extends Controller {
         var_dump($a);
         $b = $this->buildHtml("index.html", APP_PATH . 'Home/View/Cache/');
         var_dump($b);
-echo "<hr><br />";
+        echo "<hr><br />";
         $this->display('Home@Index/index');
-echo "<hr><br />";
+        echo "<hr><br />";
         $this->theme('blue');
 
         $this->display();
@@ -34,6 +40,19 @@ echo "<hr><br />";
      */
     public function bindParam($year, $mouth, $day){
 
-        echo $year ."-" . $mouth . '-' . $day;
+        echo $year . "-" . $mouth . '-' . $day;
+    }
+
+    public function view(){
+        $this->a1 = 'a-1';
+        $this->a2 = 'a-2';
+        $this->assign("b1", 'b-1');
+        $this->assign("b2", "b-2");
+        $param = [
+            'c1' => 'c-1',
+            'c2' => 'c-2'
+        ];
+        $this->assign($param);
+        $this->display();
     }
 }
