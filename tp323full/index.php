c4a7d65e45130e7c93eee6bd6185a6d1cd22dd01
componey_qss <475185283@qq.com>
2019-06-04 11:40:39 +0800

    重构整体。把原框架代码集合在一个地方 Signed-off-by: componey_qss <475185283@qq.com>

--- tp323full/index.php
+++ tp323full/index.php
@@ -21,6 +21,6 @@ define('APP_DEBUG',True);
 define('APP_PATH','./Application/');
 
 // 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
+require '../../php_frame_code/code/source/thinkphp_3.2.3_full/ThinkPHP/ThinkPHP.php';
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单
