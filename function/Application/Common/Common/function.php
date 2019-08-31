4f6a7e7a58806a218cdc9744c4b6458af4e37949
componey_qss <475185283@qq.com>
2019-08-31 11:54:50 +0800

    Signed-off-by: componey_qss <475185283@qq.com>

--- function/Application/Common/Common/function.php
+++ function/Application/Common/Common/function.php
@@ -24,3 +24,42 @@ function arrayMultiSort(&$multiArr, $column, $sort = SORT_ASC) {
     }
     return array_multisort($columnArr, $sort, $multiArr);
 }
+
+function download_images($article_url = '', $savePath = 'C:\Users\Administrator\Desktop\aa', $fileExt = ['jpg','bmp','jpeg','gif','png']){
+    if(!is_array($fileExt) || !$fileExt) return false;
+    $extStr = join("|", $fileExt);
+    $regFile = '/["|\']([^"|\']*.('.$extStr.'))["|\']/';
+    $regFile = '/["|\'](http:\/\/|http:\/\/|\/\/)?([^"|\']*.('.$extStr.'))["|\']/';
+
+    $htmlPage = file_get_contents($article_url);
+    $ret = preg_match_all($regFile, $htmlPage, $matchResult);
+
+    $fileArr = array_unique($matchResult[2]);
+
+    foreach($fileArr as $file){
+        // 获取文件信息
+        $fileInfo = getFileBinary($file);
+        if($fileInfo['file_stream'] === false) continue;
+        $filename = $savePath . '/' . uniqid() . strrchr($file, '.');
+        $local_file = fopen($filename, 'w');
+        if(false !== $local_file){
+            if( false !== fwrite($local_file, $fileInfo['file_stream']) ){
+                fclose($local_file);
+            }
+        }
+    }
+}
+
+function getFileBinary($file){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $file);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_NOBODY, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE );
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $fileInfo = curl_exec($ch);//二进制文件流
+    $httpInfo = curl_getinfo($ch);
+    curl_close($ch);
+    return ['file_stream' => $fileInfo, "http_info" => $httpInfo];
+}
