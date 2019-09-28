12046133e8f8c2b39aef0a642c0ac0975ba6846f
flancklin <475185283@qq.com>
2019-09-28 11:41:53 +0800

    Signed-off-by: flancklin <475185283@qq.com>

--- env/git.sh
+++ env/git.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-cd /www/html
-git pull origin master
 cd /www/php_self
+git checkout .
 git pull origin master
-cd /www/php_down
-git pull origin master
-cd /www/php_source
-git pull origin master
-
 time=$(date "+%Y-%m-%d %H:%M:%S")
 echo "${time}"
