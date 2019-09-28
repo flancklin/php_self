426d9dbcb558e8834e21571766160da68a18e16a
flancklin <475185283@qq.com>
2019-09-28 12:21:10 +0800

    Signed-off-by: flancklin <475185283@qq.com>

--- env/git.sh
+++ env/git.sh
@@ -4,3 +4,15 @@ git checkout .
 git pull origin master
 chmod 777 -R /www
 
+cd /www/php_down
+git checkout .
+git pull origin master
+
+cd /www/php_source
+git checkout .
+git pull origin master
+
+cd /www/html
+git checkout .
+git pull origin master
+
