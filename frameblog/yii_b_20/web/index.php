ec10388ac8d3e29101a1c5bc3ed4aa0e7c4fb15b
flancklin <475185283@qq.com>
2019-10-05 21:40:48 +0800

    Signed-off-by: flancklin <475185283@qq.com>

--- frameblog/yii_b_20/web/index.php
+++ frameblog/yii_b_20/web/index.php
@@ -7,10 +7,10 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 //require __DIR__ . '/../vendor/autoload.php';
 //require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
-var_dump(3333333);die;
 require __DIR__ . '/../../../php_source/yii-basic-app-2.0.17/basic/vendor/autoload.php';
 require __DIR__ . '/../../../php_source/yii-basic-app-2.0.17/basic/vendor/yiisoft/yii2/Yii.php';
 
+var_dump(3333333);die;
 $config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
